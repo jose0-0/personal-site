d4c283e134881101950a48cde8267569c1253ea3
Jose Arguetaq <jose.argueta@ionnovate.com>
2024-06-17 11:52:46 -0700

    built a mobile friendly nav bar

--- personal-site/components/Nav.tsx
+++ personal-site/components/Nav.tsx
@@ -1,30 +1,38 @@
-'Use Client';
+'use client';
 import Link from 'next/link';
+import { useState } from 'react'
 import { CustomButton } from '@/components';
+import { AiOutlineMenu, AiOutlineClose } from 'react-icons/ai'
 
 const links = [
   {
     name: 'Services',
-    path: '/services',
+    path: '/#services',
   },
   {
     name: 'Resume',
-    path: '/resume',
+    path: '/#resume',
   },
   {
     name: 'Projects',
-    path: '/projects',
+    path: '/#projects',
   },
 
 ]
 
 const Nav = () => {
+  const [mobileNavVisible, setMobileNavVisible] = useState(false);
+
+  const toggleNav = () => {
+    setMobileNavVisible(!mobileNavVisible);
+  };
+
   return (
     <header className='py-8 xl:py-12 text-white'>
       <div className='container mx-auto flex justify-between items-center'>
         <Link href='/' className=''>
           <h1 className='text-4xl font-semibold'>
-            Jose<span className='text-accent'>.</span>
+            JOSE<span className='text-accent'>.</span>
           </h1>
         </Link>
         {/* Desktop Nav */}
@@ -49,8 +57,28 @@ const Nav = () => {
           </nav>
         </div>
         {/* Mobile Nav */}
-        <div className='xl:hidden'> 
-          Mobile Nav 
+        <div onClick={toggleNav} className='block xl:hidden z-10'> 
+          { mobileNavVisible 
+            ? <AiOutlineClose size={20}/> 
+            : <AiOutlineMenu size={20} />
+          }
+        </div>
+        <div className={ 
+          mobileNavVisible 
+            ? 'xl:hidden absolute top-0 left-0 right-0 bottom-0 flex flex-col justify-center items-center w-full h-screen bg-black text-white ease-in duration-300' 
+            : 'xl:hidden absolute top-0 left-[-100%] right-0 bottom-0 flex flex-col justify-center items-center w-full h-screen bg-black text-white ease-in duration-300'
+          }>
+          {links.map(( link, index ) => {
+            return (
+              <Link 
+                href={link.path} 
+                key={index}
+                className='p-4 text-4xl hover:text-accent duration-300'
+              >
+                {link.name}
+              </Link>
+            )
+          })}
         </div>
       </div>
     </header>
--- personal-site/components/index.ts
+++ personal-site/components/index.ts
@@ -1,9 +1,7 @@
 import Nav from './Nav';
-import MobileNav from './MobileNav';
 import CustomButton from './CustomButton';
 
 export {
     Nav,
-    MobileNav,
     CustomButton,
 }
