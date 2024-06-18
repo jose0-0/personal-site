c7dfd38ccea4e60ac23c553b71b6cc68d49e9f70
Jose Arguetaq <jose.argueta@ionnovate.com>
2024-06-17 20:29:36 -0700

    built a service section that contains my professional services, content WIP

--- personal-site/components/index.ts
+++ personal-site/components/index.ts
@@ -1,9 +1,11 @@
 import Nav from './Nav';
 import CustomButton from './CustomButton';
 import Socials from './Socials';
+import Services from './Services';
 
 export {
     Nav,
     CustomButton,
-    Socials
+    Socials,
+    Services,
 }
